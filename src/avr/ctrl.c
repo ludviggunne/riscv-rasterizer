b7688287b580b72702c966acdb512459750a7076
Ludvig Gunne Lindström <ludvig.gunne.lindstrom.dev@proton.me>
2024-10-10 16:32:40 +0200

    add more controller stuff

--- src/avr/ctrl.c
+++ src/avr/ctrl.c
@@ -1,11 +1,39 @@
 #include <stdint.h>
 #include <avr/io.h>
 #include <util/delay.h>
+#include <string.h>
 #include "pif.h"
 
 #define CTRL_PIN  2
 #define DTEK_CLK  3
 #define DTEK_DATA 4
+
+#define RESET 0xff
+#define POLL 0x01
+
+#define THRESHOLD 2
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+int changed(uint8_t *now, uint8_t *then)
+{
+	if (now[0] != then[0])
+		return 1;
+	if (now[1] != then[1])
+		return 1;
+
+	int8_t xnow = *(int8_t*)&now[2];
+	int8_t ynow = *(int8_t*)&now[3];
+	int8_t xthen = *(int8_t*)&then[2];
+	int8_t ythen = *(int8_t*)&then[3];
+
+	if (ABS(xnow - xthen) > THRESHOLD)
+		return 1;
+	if (ABS(ynow - ythen) > THRESHOLD)
+		return 1;
+
+	return 0;
+}
 
 void send(uint8_t *data)
 {
@@ -36,7 +64,8 @@ int main(int argc, char **argv)
 	(void)argc;
 	(void)argv;
 
-	uint8_t recvbuf[4], sendbuf[4];
+	uint8_t recvbuf[4];
+	uint8_t recvbuf_prev[4] = { 0 };
 
 	// configure pins
 	DDRB |= _BV(DTEK_CLK);
@@ -44,5 +73,17 @@ int main(int argc, char **argv)
 	PORTB &= ~_BV(DTEK_CLK);
 	PORTB &= ~_BV(DTEK_DATA);
 
-	pif_host_transmit(CTRL_PIN, 0, 0, 0, 0);
+	// reset controller
+	uint8_t request = RESET;
+	pif_host_transmit(CTRL_PIN, &request, 1, recvbuf, 3);
+
+	for (;;)
+	{
+		request = POLL;
+		pif_host_transmit(CTRL_PIN, &request, 1, recvbuf, 4);
+		if (changed(recvbuf, recvbuf_prev))
+			send(recvbuf);
+		_delay_ms(33);
+		memcpy(recvbuf_prev, recvbuf, sizeof(*recvbuf));
+	}
 }
