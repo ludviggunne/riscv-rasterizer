25b7cfb8a4236e94458cfe4bf00fd0ccdae9c0d5
Ludvig Gunne Lindström <ludvig.gunne.lindstrom.dev@proton.me>
2024-10-13 13:17:05 +0200

    format

--- src/dtekv/timer.c
+++ src/dtekv/timer.c
@@ -6,18 +6,17 @@
 
 void timer_init(void)
 {
-  *TIMER_BASE = 1; // reset TO if already set
-  *(TIMER_BASE+1) = 8; // stop counter if it's already running
-  *(TIMER_BASE+1) = 3; // set CONT bit, enable interrupts
-
+	*TIMER_BASE = 1; // reset TO if already set
+	*(TIMER_BASE+1) = 8; // stop counter if it's already running
+	*(TIMER_BASE+1) = 3; // set CONT bit, enable interrupts
 }
 
 void timer_start(unsigned int ms)
 {
-  unsigned int counter = ms * FREQ / 1000;
-  *(TIMER_BASE+2) = counter & 0xffff;
-  *(TIMER_BASE+3) = counter >> 16;
-  *(TIMER_BASE+1) = 4;
+	unsigned int counter = ms * FREQ / 1000;
+	*(TIMER_BASE+2) = counter & 0xffff;
+	*(TIMER_BASE+3) = counter >> 16;
+	*(TIMER_BASE+1) = 4;
 }
 
 void timer_clear_interrupt(void)
